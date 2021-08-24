edc7f560daa5d4e569afcbf8a2ff40ee6dcea259
baichao ling <1585638808@qq.com>
2021-08-24 14:52:59 +0800

    Update codegen.js

--- example/swagger/codegen.js
+++ example/swagger/codegen.js
@@ -3,8 +3,9 @@ const { codegen } = require('../../dist/index.js')
 
 codegen({
   methodNameMode: 'path',
-  source: require('../swagger.json'),
+  // source: require('../swagger.json'),
   // remoteUrl: 'http://localhost:44307/swagger/v1/swagger.json',
+  remoteUrl: 'http://192.168.1.60:7888/api-system/v2/api-docs',
   outputDir: './swagger/services',
   strictNullChecks: false,
   // useCustomerRequestInstance: true,
